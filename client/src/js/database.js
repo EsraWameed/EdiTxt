7b8e09f61a95865fe86cee1d560c4d9d72643769
Esra Al <esra.wameed@yahoo.com>
2022-11-11 16:02:07 -0500

    added starter code

---
+++ client/src/js/database.js
@@ -0,0 +1,33 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+console.error('putDb not implemented');
+//connection to the db and version we want to use
+ // Create a connection to the database database and version we want to use.
+ const contactDb = await openDB('jate', 1);
+ //created a new transaction to specify db and data priviledge
+ const tx = contactDb.transaction('jate', 'readwrite');
+//opening the desired object store
+const store = tx.objectStore('jate');
+//using .add() to store and pass in content
+const request = store.add({ content: content });
+
+
+}
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error('getDb not implemented');
+
+initdb();
